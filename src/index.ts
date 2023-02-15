5b736db97b5a306e42b8491cd675b43b4f7ebef0
shanghaojia <1448031411@qq.com>
2023-02-15 16:35:33 +0800

    first commit

---
+++ src/index.ts
@@ -0,0 +1,85 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `wrangler dev src/index.ts` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+export interface Env {
+	'url': D1Database;
+}
+async function randomString() {
+	let len = 6;
+	let $chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678';    /****默认去掉了容易混淆的字符oOLl,9gq,Vv,Uu,I1****/
+	let maxPos = $chars.length;
+	let result = '';
+	for (let i = 0; i < len; i++) {
+		result += $chars.charAt(Math.floor(Math.random() * maxPos));
+	}
+	return result;
+}
+
+async function sha512(url: string) {
+	let url1 = new TextEncoder().encode(url)
+
+	const url_digest = await crypto.subtle.digest(
+		{
+			name: "SHA-512",
+		},
+		url1, // The data you want to hash as an ArrayBuffer
+	)
+	const hashArray = Array.from(new Uint8Array(url_digest)); // convert buffer to byte array
+	const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+	//console.log(hashHex)
+	return hashHex
+}
+
+
+async function checkURL(URL: string) {
+	let str = URL;
+	let Expression = /http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- .\/?%&=]*)?/;
+	let objExp = new RegExp(Expression);
+	if (objExp.test(str) == true) {
+		if (str[0] == 'h')
+			return true;
+		else
+			return false;
+	} else {
+		return false;
+	}
+}
+
+export default {
+	async fetch(request: Request, env: Env) {
+		const { pathname } = new URL(request.url);
+		if(pathname=='/shorten'){
+			const url=await request.text();
+			if(await checkURL(url)){
+				let hash=await randomString();
+				await env.url.prepare(
+					"INSERT INTO urls VALUES (?,?)"
+				  ).bind(hash,url).run();
+				return new Response(hash);
+			}
+			else{
+				return new Response('你这url有问题啊');
+			}
+		}else{
+			const hash=pathname.substring(1);
+			const { results } = await env.url.prepare(
+				"SELECT * FROM urls WHERE hash=?"
+			  ).bind(hash).all();
+			  if(results.length==0){
+				  return new Response("404");
+			  }else{
+				  return Response.redirect(results[0].url);
+			  }
+		}
+
+	}
+
+
+}
